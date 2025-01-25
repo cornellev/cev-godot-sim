468eb5804b77861ac630920408ed45ea10b56657
Myles Pasetsky <myles.pasetsky@gmail.com>
2025-01-25 13:44:55 -0500

    Improve IMU sensor (at least on current car model)

--- imu.gd
+++ imu.gd
@@ -10,13 +10,28 @@ func _ready() -> void:
 	publisher = GodotRosImuPublisher.new();
 	publisher.init(Global.ros2_node, topic_name, 10);
 
+var previous_velocity: Vector3 = Vector3.ZERO
+var acceleration: Vector3 = Vector3.ZERO
+
+#var previous_ang_velocity: float
+#var ang_acceleration: float
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var racecar = $"../Racecar"
+	#racecar.t
 	var imu = Imu.new();
 	
-	# TODO: get Imu data.
+	var velocity = racecar.velocity
+	acceleration = (velocity - previous_velocity) / delta;
+	previous_velocity = velocity;
+	
+	var ang_velocity = racecar.angular_velocity
+	#ang_acceleration = (angular_velocity - previous_ang_velocity) / delta
+	#previous_ang_velocity = ang_velocity
+
 	imu.set_frame_id(frame_id);
-	imu.set_linear_acceleration(Vector3(1.0, 2.0, 3.0));
+	imu.set_linear_acceleration(acceleration);
+	imu.set_angular_velocity(Vector3(0.0, ang_velocity, 0.0));
 	
 	publisher.publish(imu);
--- racecar.gd
+++ racecar.gd
@@ -10,13 +10,26 @@ var yaw = 0.0
 
 var velocity_forward: float = 0.0
 
+var dstate: Vector3
+
+var prev_position: Vector3 = Vector3.ZERO
+var angular_velocity: float
+
+
+
 func _physics_process(delta: float) -> void:
 	# Inputs
 	var speed = MAX_SPEED * InputSource.get_speed()
 	var steering = MAX_STEERING * InputSource.get_steering()
-#
-	rotation.y += speed * tan(steering) / WHEELBASE * delta
-	var dstate = Vector3(speed, 0, 0).rotated(Vector3.UP, rotation.y) * delta
+#	
+	angular_velocity = speed * tan(steering) / WHEELBASE
+	rotation.y += angular_velocity * delta
+	dstate = Vector3(speed, 0, 0).rotated(Vector3.UP, rotation.y) * delta
 	velocity_forward = speed;
-
+	
+	velocity = (position - prev_position) / delta
+	prev_position = position;
+	
+	#velocity = dstate/delta;
 	move_and_collide(dstate)
+	#move_and_slide();
